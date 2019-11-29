5a174cfaa60a13484d6f0d67217d0fdcb0de7f93
Cameron Gorrie <sastraxi@gmail.com>
2019-11-28 22:12:19 -0500

    remove debug logging

--- src/server.ts
+++ src/server.ts
@@ -57,7 +57,6 @@ try {
 
     app.post('/', async (req, res) => {
       const body: string = Array.isArray(req.body) ? req.body.join('\n') : req.body;
-      console.log(req.body);
 
       const signature = req.headers["x-pgsh-signature"];
       if (!signature || hmac(body) !== signature) {
